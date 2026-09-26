06ca1e0eece73bc2eee2c3309ba8f02d70772c18
Niyaz Fazlyev <nya.nya.nyaz@mail.ru>
2026-09-26 04:36:54 +0300

    Сделана п1.3.6

---
+++ 134-led-module/logging/log.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdio.h>
+
+#define DEVICE_NAME "es-led-module"
+#define FIRMWARE_VERSION "1.0.0"
+
+#define LOG_LEVEL_ERR 1
+#define LOG_LEVEL_INF 2
+#define LOG_LEVEL_DBG 3
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_LEVEL_DBG
+#endif
+
+void log_version(void);
+void log_prefix(const char *level, const char *function, int line);
+
+
+#define LOG(level, ...)                             \
+    do                                              \
+    {                                               \
+        if (LOG_LEVEL >= level)                     \
+        {                                           \
+            log_prefix("inf", __func__, __LINE__);  \
+            printf(__VA_ARGS__);                    \
+        }                                           \
+    } while (0)
+
+#define LOG_ERR(...) LOG(LOG_LEVEL_ERR, __VA_ARGS__)  
+#define LOG_INF(...) LOG(LOG_LEVEL_INF, __VA_ARGS__)  
+#define LOG_DBG(...) LOG(LOG_LEVEL_DBG, __VA_ARGS__)  
--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -2,6 +2,7 @@
 
 #include "hardware/gpio.h"
 #include "led.h"
+#include "log.h"
 #include "pico/stdlib.h"
 
 const uint BUTTON_PIN = 15;
@@ -16,12 +17,14 @@ bool get_button_debounce(uint pin) {
 void handle_command(int command) {
   if (command == 'e') {
     led_set(true);
-    printf("led %s\n", led_is_on() ? "on" : "off");
+    LOG_INF("led %s\n", led_is_on() ? "on" : "off");
   } else if (command == 'd') {
     led_set(false);
-    printf("led %s\n", led_is_on() ? "on" : "off");
+    LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+  } else if (command == 'v') {
+    log_version();
   } else {
-    printf("unknown command: %c\n", command);
+    LOG_ERR("unknown command: %c\n", command);
   }
 }
 
@@ -41,12 +44,13 @@ int main() {
     current = get_button_debounce(BUTTON_PIN);
     if (prev == true && current == false) {
       led_toggle();
-      printf("led %s\n", led_is_on() ? "on" : "off");
+    LOG_INF("led %s\n", led_is_on() ? "on" : "off");
     }
     prev = current;
 
     int command = getchar_timeout_us(0);
     if (command == PICO_ERROR_TIMEOUT) continue;
+    LOG_DBG("got %c\n", command);
     handle_command(command);
   }
 }
